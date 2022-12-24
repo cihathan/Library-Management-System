1bb1d430a3ad00a5e5cae31b419f59b66fc98e04
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2022-12-24 15:25:19 +0300

    AddAuthor, AddCategory, GetCategory views and AuthorController, BookController, CategoryController have been added.

---
+++ MvcLibrary/MvcLibrary/Controllers/AuthorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLibrary.Models.Entity;
+namespace MvcLibrary.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        devrimme_cihatEntities db = new devrimme_cihatEntities();
+        public ActionResult Index()
+        {
+            var values=db.Author.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddAuthor()
+        {
+            return View();
+        }
+        public ActionResult AddAuthor(Author author)
+        {
+            db.Author.Add(author);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
--- MvcLibrary/MvcLibrary/Controllers/CategoryController.cs
+++ MvcLibrary/MvcLibrary/Controllers/CategoryController.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcLibrary.Models.Entity;
 
 namespace MvcLibrary.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        devrimme_cihatEntities db = new devrimme_cihatEntities();
         public ActionResult Index()
         {
+            var values = db.Category.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            db.Category.Add(category);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = db.Category.Find(id);
+            db.Category.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult GetCategory(int id)
+        {
+            var category = db.Category.Find(id);
+            return View("GetCategory", category);
+        }
+        public ActionResult UpdateCategory(Category category)
+        {
+            var ctg = db.Category.Find(category.ID);
+            ctg.Name = category.Name;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
