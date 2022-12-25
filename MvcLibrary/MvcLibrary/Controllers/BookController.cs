72290dcbf06a8248400623de197d74be9f58b511
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2022-12-25 19:49:56 +0300

    Added function to list books.

--- MvcLibrary/MvcLibrary/Controllers/BookController.cs
+++ MvcLibrary/MvcLibrary/Controllers/BookController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcLibrary.Models.Entity;
 
 namespace MvcLibrary.Controllers
 {
     public class BookController : Controller
     {
         // GET: Book
+        devrimme_cihatEntities db = new devrimme_cihatEntities();
         public ActionResult Index()
         {
-            return View();
+            var books = db.Book.ToList();
+            return View(books);
         }
     }
 }
