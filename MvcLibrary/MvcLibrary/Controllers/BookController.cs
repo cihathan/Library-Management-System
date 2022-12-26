1df8897b37a998ca327389170619fc664ac1c42c
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2022-12-26 18:30:43 +0300

    Added book add function.

--- MvcLibrary/MvcLibrary/Controllers/BookController.cs
+++ MvcLibrary/MvcLibrary/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 using MvcLibrary.Models.Entity;
@@ -15,6 +16,20 @@ namespace MvcLibrary.Controllers
         {
             var books = db.Book.ToList();
             return View(books);
+        }
+        [HttpGet]
+        public ActionResult AddBook()
+        {
+            List<SelectListItem> categoryValue = (from i in db.Category.ToList() select new SelectListItem { Text = i.Name, Value = i.ID.ToString() }).ToList();
+            ViewBag.cValue = categoryValue;
+            List<SelectListItem> authorValue = (from i in db.Author.ToList() select new SelectListItem { Text = i.Name + i.LastName, Value = i.Name.ToString() }).ToList();
+            ViewBag.aValue = authorValue;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddBook(Book book)
+        {
+            return View();
         }
     }
 }
