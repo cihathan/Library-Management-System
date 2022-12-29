17c225661e92550af4bbf632813519dbb40e3825
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2022-12-29 15:27:56 +0300

    Added user listing functionality.

---
+++ MvcLibrary/MvcLibrary/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLibrary.Models.Entity;
+namespace MvcLibrary.Controllers
+{
+    public class UserController : Controller
+    {
+        devrimme_cihatEntities db = new devrimme_cihatEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            var values = db.Users.ToList();
+            return View(values);
+        }
+    }
+}
