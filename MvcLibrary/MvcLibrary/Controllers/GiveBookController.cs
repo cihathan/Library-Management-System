bfab33dceff39766de85bffaea30756be8bc0ed9
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2022-12-29 17:35:45 +0300

    Page design for book giving function.

---
+++ MvcLibrary/MvcLibrary/Controllers/GiveBookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLibrary.Controllers
+{
+    public class GiveBookController : Controller
+    {
+        // GET: GiveBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GiveBook() {
+            return View();
+        }
+    }
+}
