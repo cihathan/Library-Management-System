f2fc8dff7fe24a9a2b0227042f58c6e742aa3454
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2022-12-29 17:57:31 +0300

    Added the function of giving a book to the member.

--- MvcLibrary/MvcLibrary/Controllers/GiveBookController.cs
+++ MvcLibrary/MvcLibrary/Controllers/GiveBookController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcLibrary.Models.Entity;
 namespace MvcLibrary.Controllers
 {
     public class GiveBookController : Controller
     {
+        devrimme_cihatEntities db = new devrimme_cihatEntities();
         // GET: GiveBook
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public ActionResult GiveBook() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiveBook(Movement p)
+        {
+            db.Movement.Add(p);
+            db.SaveChanges();
             return View();
         }
     }
